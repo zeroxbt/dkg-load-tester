c3de25f134ff91b76bde1cd3726d4deb29508d2a
zeroxbt <original.swazi@protonmail.com>
2022-07-27 18:11:07 +0200

    update according to new dkg client spec

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require("dotenv").config();
-const { setTimeout } = require("timers/promises");
-const randomWords = require("random-words");
-const DkgClient = require("dkg.js");
-const loadModels = require("./repository-service");
-const endpoints = require("./endpoints.json");
+import "dotenv/config";
+import { setTimeout } from "timers/promises";
+import { DkgClient } from "dkg.js/index-new.js";
+import { readFileSync } from "fs";
+
+// const loadModels = require("./repository-service");
+const endpoints = JSON.parse(readFileSync("./endpoints.json"));
 
 let models;
 const sleepSeconds = 60;
@@ -12,104 +13,140 @@ const clients = endpoints.map(
   (endpoint) =>
     new DkgClient({
       endpoint,
-      useSSL: true,
       port: 8900,
-      loglevel: "info",
+      communicationType: "http",
+      useSSL: true,
+      loglevel: "trace",
+      blockchain: "ethereum",
+      blockchainConfig: {
+        ethereum: {
+          rpc: "http://localhost:7545",
+          hubContract: "0xdaa16AC171CfE8Df6F79C06E7EEAb2249E2C9Ec8",
+          wallet: process.env.PUBLIC_KEY,
+          privateKey: process.env.PRIVATE_KEY,
+        },
+      },
     })
 );
 
+const logDivider = () => {
+  console.log(
+    "-------------------------------------------------------------------------------------------------"
+  );
+};
+
+const getRandomClient = (operation) => {
+  const clientIndex = Math.floor(Math.random() * clients.length);
+  const hostname = endpoints[clientIndex];
+  console.log(`Calling ${operation} from endpoint : ${hostname}`);
+  return { hostname, client: clients[clientIndex] };
+};
+
+const updateRepository = (
+  operation,
+  operationResult,
+  ual,
+  assertionId,
+  hostname,
+  operationStart,
+  operationEnd
+) => {
+  models[`script_${operation}`].create({
+    handler_id: operationResult?.operation_id ?? "",
+    status: operationResult?.status ?? "FAILED",
+    created_at: Date.now(),
+    hostname,
+    ual,
+    assertion_id: assertionId,
+    start_timestamp: operationStart,
+    end_timestamp: operationEnd,
+  });
+};
+
 const publish = async () => {
+  logDivider();
+
   const content = {
     "@context": "https://schema.org",
     "@type": "Person",
     name: "John Doe",
+    identifier: Math.floor(Math.random() * 1e10),
   };
 
-  const publishOptions = {
+  let publishOptions = {
     visibility: "public",
+    holdingTimeInYears: 1,
+    tokenAmount: 10,
+    blockchain: "ethereum",
+    wallet: process.env.PUBLIC_KEY,
+    maxNumberOfRetries: 5,
   };
 
-  const clientIndex = Math.floor(Math.random() * clients.length);
-  const randomClient = clients[clientIndex];
-  publishOptions.keywords = randomWords(5);
-
-  console.log(
-    "-------------------------------------------------------------------------------------------------"
-  );
-  console.log(`Publishing from endpoint : ${endpoints[clientIndex]}`);
-  const publishStart = Date.now();
-  const publishResult = await randomClient.assets
-    .create(content, publishOptions, {
-      publicKey: process.env.PUBLIC_KEY,
-      privateKey: process.env.PRIVATE_KEY,
-    })
+  const { client, hostname } = getRandomClient("publish");
+  const start = Date.now();
+  const publishResult = await client.asset
+    .create(content, publishOptions)
     .catch((e) => console.log(`Publishing error : ${e.message}`));
-  const publishEnd = Date.now();
+  const end = Date.now();
   console.log(`Publish result : ${JSON.stringify(publishResult, null, 2)}`);
-  console.log(
-    "-------------------------------------------------------------------------------------------------"
-  );
 
-  models.script_publish.create({
-    handler_id: publishResult?.handler_id ?? "",
-    status: publishResult?.status ?? "FAILED",
-    created_at: Date.now(),
-    hostname: endpoints[clientIndex],
-    ual: publishResult?.data?.ual,
-    assertion_id: publishResult?.data?.assertionId,
-    start_timestamp: publishStart,
-    end_timestamp: publishEnd,
-  });
+  /* updateRepository(
+    "publish",
+    publishResult,
+    publishResult.UAL,
+    publishResult.assertionId,
+    hostname,
+    start,
+    end
+  ); */
+
+  logDivider();
 
   return publishResult;
 };
 
-const resolve = async (ual, assertionId) => {
-  const resolveOptions = {
+const get = async (ual, assertionId) => {
+  logDivider();
+
+  let getOptions = {
+    validate: true,
     outputFormat: "json-ld",
-    validateOutput: true,
+    commitOffset: 0,
+    blockchain: "ethereum",
+    maxNumberOfRetries: 5,
   };
-  const clientIndex = Math.floor(Math.random() * clients.length);
-  const randomClient = clients[clientIndex];
+  const { client, hostname } = getRandomClient("resolve");
 
-  console.log(
-    "-------------------------------------------------------------------------------------------------"
-  );
-  console.log(`Resolving from endpoint : ${endpoints[clientIndex]}`);
-  const resolveStart = Date.now();
-  const resolveResult = await randomClient
-    .resolve(`${ual}/${assertionId}`, resolveOptions)
+  const start = Date.now();
+  const resolveResult = await client.asset
+    .get(ual, getOptions)
     .catch((e) => console.log(`Resolving error : ${e.message}`));
-  const resolveEnd = Date.now();
-  console.log(`Resolve result : ${JSON.stringify(resolveResult, null, 2)}`);
-  console.log(
-    "-------------------------------------------------------------------------------------------------"
-  );
+  const end = Date.now();
 
-  models.script_resolve.create({
-    handler_id: resolveResult?.handler_id ?? "",
-    status: resolveResult?.status ?? "FAILED",
-    created_at: Date.now(),
-    hostname: endpoints[clientIndex],
-    ual: ual,
-    assertion_id: assertionId,
-    start_timestamp: resolveStart,
-    end_timestamp: resolveEnd,
-  });
+  console.log(`Resolve result : ${JSON.stringify(resolveResult, null, 2)}`);
+
+  /* updateRepository(
+    "resolve",
+    publishResult,
+    ual,
+    assertionId,
+    hostname,
+    start,
+    end
+  ); */
+
+  logDivider();
 };
 
 (async () => {
-  models = await loadModels();
+  // models = await loadModels();
   while (true) {
     const publishResult = await publish();
-    if (
-      publishResult &&
-      publishResult.data &&
-      publishResult.data.ual &&
-      publishResult.data.assertionId
-    ) {
-      await resolve(publishResult.data.ual, publishResult.data.assertionId);
+
+    if (publishResult && publishResult.status === "COMPLETED") {
+      await get(publishResult.UAL, publishResult.assertionId);
     }
+
     await setTimeout(1000 * sleepSeconds);
   }
 })();

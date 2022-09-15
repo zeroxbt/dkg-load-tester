0b7b704b6d5d2a02787bb1562e824f9b11530009
zeroxbt <original.swazi@protonmail.com>
2022-09-15 16:19:29 +0200

    fix endpoints import

--- src/util/Endpoint.js
+++ src/util/Endpoint.js
@@ -1,4 +1,4 @@
-const endpoints = require("./endpoints.json");
+const endpoints = require("../../endpoints.json");
 
 module.exports.getRandomEndpoint = () => {
   return endpoints[Math.floor(Math.random() * endpoints.length)];

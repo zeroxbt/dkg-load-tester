a75c5a511124e0b04f5c4e9357571179a9edc839
zeroxbt <original.swazi@protonmail.com>
2022-09-26 16:34:04 +0200

    publish from one api at the time

--- index.js
+++ index.js
@@ -5,37 +5,34 @@ const { getRandomEndpoints } = require("./src/util/Endpoint");
 const apis = require("./src/util/apis");
 const wallets = require("./wallets.json");
 
-async function queryNode(otnode, endpoint, wallet) {
-  for (const api of apis) {
-    // fetch data
-    const data = await api.getData();
+async function queryNode(otnode, endpoint, wallet, api) {
+  // fetch data
+  const data = await api.getData();
+  // create asset
+  const publishResult = await otnode.publish(data, endpoint, wallet);
 
-    // create asset
-    const publishResult = await otnode.publish(data, endpoint, wallet);
-
-    // get asset
-    if (publishResult?.UAL) {
-      await otnode.get(publishResult.UAL, endpoint, wallet);
-    }
-
-    // sleep 5 seconds
-    await setTimeout(5 * 1000);
+  // get asset
+  if (publishResult?.UAL) {
+    await otnode.get(publishResult.UAL, endpoint, wallet);
   }
+
+  // sleep 5 seconds
+  await setTimeout(5 * 1000);
 }
 
 (async () => {
-  const clients = [0, 0, 0, 0];
-  await Promise.all(
-    clients.map((c) => new OTNode()).map((c) => c.initialize())
-  );
+  const clients = [];
+  for (let i = 0; i < 4; i++) {
+    const otnode = new OTNode();
+    await otnode.initialize();
+    clients.push(otnode);
+  }
 
   while (true) {
     const promises = [];
     const endpoints = getRandomEndpoints(4);
     for (let i = 0; i < 4; i++) {
-      promises.push(
-        queryNode(clients[i], endpoints[i], wallets[i])
-      );
+      promises.push(queryNode(clients[i], endpoints[i], wallets[i], apis[i]));
     }
     await Promise.all(promises);
   }

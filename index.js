8bd502e9126fa3a062f8335d143a1ef6a6b5c0bc
zeroxbt <original.swazi@protonmail.com>
2022-09-26 17:37:37 +0200

    add try catch

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ const wallets = require("./wallets.json");
 
 function getOverflow(array, index) {
   const i = index % array.length;
-  return array[i]
+  return array[i];
 }
 
 async function queryNode(otnode, endpoint, wallet, api) {
-  // fetch data
-  const data = await api.getData();
-  // create asset
-  const publishResult = await otnode.publish(data, endpoint, wallet);
+  try {
+    // fetch data
+    const data = await api.getData();
+    // create asset
+    const publishResult = await otnode.publish(data, endpoint, wallet);
 
-  // get asset
-  if (publishResult?.UAL) {
-    await otnode.get(publishResult.UAL, endpoint, wallet);
+    // get asset
+    if (publishResult?.UAL) {
+      await otnode.get(publishResult.UAL, endpoint, wallet);
+    }
+
+    // sleep 5 seconds
+    await setTimeout(5 * 1000);
+  } catch (e) {
+    console.log(e);
   }
-
-  // sleep 5 seconds
-  await setTimeout(5 * 1000);
 }
 
 (async () => {
@@ -38,7 +42,12 @@ async function queryNode(otnode, endpoint, wallet, api) {
     const endpoints = getRandomEndpoints(8);
     for (let i = 0; i < 8; i++) {
       promises.push(
-        queryNode(clients[i], endpoints[i], getOverflow(wallets, i), getOverflow(apis, i))
+        queryNode(
+          clients[i],
+          endpoints[i],
+          getOverflow(wallets, i),
+          getOverflow(apis, i)
+        )
       );
     }
     await Promise.all(promises);

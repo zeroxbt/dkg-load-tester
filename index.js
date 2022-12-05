0b72fe7ae6d2ea90b7bc47625072a95ccb62fd20
zeroxbt <original.swazi@protonmail.com>
2022-12-05 09:45:35 +0100

    always publish same dataset

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { setTimeout } = require("timers/promises");
 const OTNode = require("./src/apis/OTNode");
 const { getRandomEndpoints } = require("./src/util/Endpoint");
-const apis = require("./src/util/apis");
 const wallets = require("./wallets.json");
 
 const CONCURRENCY = 4;
@@ -13,31 +12,32 @@ const CONCURRENCY = 4;
   const otnode = new OTNode();
   await otnode.initialize();
 
-  let walletIndex = 0;
   while (true) {
-    for (const api of apis) {
-      // fetch data
-      const data = await api.getData();
-      const endpoints = getRandomEndpoints(concurrency);
-      const clientsOptions = [];
-      for (let i = 0; i < concurrency; ++i) {
-        clientsOptions.push({ wallet: wallets[i], endpoint: endpoints[i] });
-      }
-
-      await Promise.all(
-        clientsOptions.map(async ({ wallet, endpoint }) => {
-          // create asset
-          const publishResult = await otnode.publish(data, endpoint, wallet);
-
-          // get asset
-          if (publishResult?.operation?.status === "COMPLETED") {
-            await otnode.get(publishResult.UAL, endpoint, wallet);
-          }
-
-          // sleep 5 seconds
-          await setTimeout(5 * 1000);
-        })
-      );
+    const data = {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      name: "John Doe",
+      identifier: Math.floor(Math.random() * 1e10),
+    };
+    const endpoints = getRandomEndpoints(concurrency);
+    const clientsOptions = [];
+    for (let i = 0; i < concurrency; ++i) {
+      clientsOptions.push({ wallet: wallets[i], endpoint: endpoints[i] });
     }
+
+    await Promise.all(
+      clientsOptions.map(async ({ wallet, endpoint }) => {
+        // create asset
+        const publishResult = await otnode.publish(data, endpoint, wallet);
+
+        // get asset
+        if (publishResult?.operation?.status === "COMPLETED") {
+          await otnode.get(publishResult.UAL, endpoint, wallet);
+        }
+
+        // sleep 5 seconds
+        await setTimeout(5 * 1000);
+      })
+    );
   }
 })();
